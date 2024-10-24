6770ec5f4bcb818426a199aa4b4662d620f21fc0
lucasilliano <lucas.illiano@hotmail.com>
2024-10-24 11:09:00 +0200

    adjustment on responsive for mobile small screen

--- src/components/Educations/Education/Education.tsx
+++ src/components/Educations/Education/Education.tsx
@@ -16,11 +16,11 @@ const Education: React.FC<EducationProps> = ({ title, date, school, location, de
             <div className="paragraph_header">
                 <h2 className="title">{title}</h2>
                 <div className="details">
-                    <p>{date}</p>
+                    <p className="details_text">{date}</p>
                     <div className="vertical_line"></div>
-                    <p>{school}</p>
+                    <p className="details_text">{school}</p>
                     <div className="vertical_line"></div>
-                    <p>{location}</p>
+                    <p className="details_text">{location}</p>
                 </div>
                 <ul className="paragraph_description">
                     {Object.values(description).map((item: string, index: number) => (
